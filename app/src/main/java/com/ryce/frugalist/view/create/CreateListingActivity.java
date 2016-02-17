02b6e7ae02791157eb58391b7ac221467ddee1cb
Joseph Yen <Joseph Yen>
2016-02-16 23:41:07 -0500

    camera crash fixed

--- app/src/main/java/com/ryce/frugalist/view/create/CreateListingActivity.java
+++ app/src/main/java/com/ryce/frugalist/view/create/CreateListingActivity.java
@@ -1,21 +1,37 @@
 package com.ryce.frugalist.view.create;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.ryce.frugalist.R;
+import android.graphics.Bitmap;
+
+import java.io.File;
+import java.util.Date;
+
+import static android.graphics.Movie.decodeFile;
 
 /**
  * Created by shivand on 2/15/2016.
  */
 public class CreateListingActivity extends AppCompatActivity {
+
+    ImageView iv;
+    final private int CAPTURE_IMAGE = 2;
+    private String imgPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +41,39 @@ public class CreateListingActivity extends AppCompatActivity {
         if (savedInstanceState == null) {
             //getSupportFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
         }
-        ImageView iv = (ImageView) findViewById(R.id.cameraView);
+        iv = (ImageView) findViewById(R.id.cameraView);
         iv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 0);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, setImageUri());
+                startActivityForResult(intent, CAPTURE_IMAGE);
             }
         });
+    }
+
+    public Uri setImageUri() {
+        // Store image in dcim
+        File file = new File(Environment.getExternalStorageDirectory() + "/DCIM/", "image" + new Date().getTime() + ".png");
+        Uri imgUri = Uri.fromFile(file);
+        this.imgPath = file.getAbsolutePath();
+        return imgUri;
+    }
+
+
+    public String getImagePath() {
+        return imgPath;
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        if (resultCode != Activity.RESULT_CANCELED) {
+            if (requestCode == CAPTURE_IMAGE) {
+                String selectedImagePath = getImagePath();
+                iv.setImageBitmap(BitmapFactory.decodeFile(selectedImagePath));
+            } else {
+                super.onActivityResult(requestCode, resultCode, data);
+            }
+        }
     }
 
     @Override
@@ -47,19 +88,23 @@ public class CreateListingActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
+    /**
+     +     * A placeholder fragment containing a simple view.
+     +     */
+    public static class PlaceholderFragment extends Fragment {
         public PlaceholderFragment() { }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.activity_posting,
-                    container, false);
-            return rootView;
-        }
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+             View rootView = inflater.inflate(R.layout.activity_posting, container, false);
+                return rootView;
+             }
     }
+
+
 }
