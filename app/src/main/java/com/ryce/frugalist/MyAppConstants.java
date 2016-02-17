0cca9976211dbbc4c570d6be1ab50de48256d2d1
Roger Wang <RW_PlayOnWords@yahoo.ca>
2016-02-17 15:55:51 -0500

    added the imgur stuff, without integrating fully has some comments

---
+++ app/src/main/java/com/ryce/frugalist/MyAppConstants.java
@@ -0,0 +1,9 @@
+package com.ryce.frugalist;
+
+public class MyAppConstants {
+    public static final String MY_IMGUR_CLIENT_ID = "c09bf20a6cf8625";
+    public static final String MY_IMGUR_CLIENT_SECRET = "157993c6e164975a7915974b5c7a340ecf39e0aa";
+    //The arbitrary redirect url (Authorization callback URL), ex. awesome://imgur or http://android,
+    // declared when registering your app with Imgur API
+    public static final String MY_IMGUR_REDIRECT_URL = "FrugaList";
+}
